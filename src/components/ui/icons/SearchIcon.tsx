469d34d9441b139b3d0ea3344fa7bd8e6bc1cebf
Sutaenghhh <neocop7@gmail.com>
2025-03-20 19:38:54 +0900

    style:header및 nav바 수정

--- src/components/ui/icons/SearchIcon.tsx
+++ src/components/ui/icons/SearchIcon.tsx
@@ -3,19 +3,22 @@ import React from 'react';
 function searchIcon() {
   return (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
       fill="none"
-      stroke="currentColor"
-      stroke-Width="2"
-      stroke-Linecap="round"
-      stroke-Linejoin="round"
-      className="lucide lucide-search"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx="11" cy="11" r="8" />
-      <path d="m21 21-4.3-4.3" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M7.6001 14.8C7.6001 18.76 10.8401 22 14.8001 22C18.7601 22 22.0001 18.76 22.0001 14.8C22.0001 10.84 18.7601 7.60001 14.8001 7.60001C10.8401 7.60001 7.6001 10.84 7.6001 14.8ZM8.80013 14.8C8.80013 11.44 11.4401 8.80001 14.8002 8.80001C18.1602 8.80001 20.8002 11.44 20.8002 14.8C20.8002 18.16 18.1602 20.8 14.8002 20.8C11.4401 20.8 8.80013 18.16 8.80013 14.8Z"
+        fill="#666767"
+      />
+      <path
+        d="M19.0188 19.8562L19.8673 19.0077L25.3827 24.5231L24.5342 25.3716L19.0188 19.8562Z"
+        fill="#666767"
+      />
     </svg>
   );
 }
