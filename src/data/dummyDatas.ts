7fbd8d7dea5e8a79c9be25eac79ea2f768203c2e
unknown <doeh2580@gmail.com>
2025-03-20 15:38:02 +0900

    feat: 메인 페이지 슬라이드 배너 추가 ResponseDataTypes.ts 파일을 생성하여 슬라이드 배너에 대한 타입을 정의하고 더미 데이터를 만들어 적용함.

---
+++ src/data/dummyDatas.ts
@@ -0,0 +1,19 @@
+import { BannerSlideImageType } from '@/types/ResponseDataTypes';
+
+export const mainBannerSlideData: BannerSlideImageType[] = [
+  {
+    id: 1,
+    description: 'main banner 1',
+    imgUrl: 'https://picsum.photos/id/237/390/330',
+  },
+  {
+    id: 2,
+    description: 'main banner 2',
+    imgUrl: 'https://picsum.photos/id/38/380',
+  },
+  {
+    id: 3,
+    description: 'main banner 3',
+    imgUrl: 'https://picsum.photos/id/58/380',
+  },
+];
