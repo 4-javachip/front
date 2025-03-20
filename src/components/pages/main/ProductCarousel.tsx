c14d9efa9ce0ec7c68fb567f4a12e80f1eba3f96
unknown <doeh2580@gmail.com>
2025-03-20 16:41:47 +0900

    chore: font 파일 수정 기존 weight대로 나누어 놓았던 font 파일을 하나의 regular 파일로 통합함

--- src/components/pages/main/ProductCarousel.tsx
+++ src/components/pages/main/ProductCarousel.tsx
@@ -3,7 +3,7 @@ import ProductCarouselItem from './ProductCarouselItem';
 export default function ProductCarousel() {
   return (
     <div className="flex flex-col container overflow-x-auto scrollbar gap-[30px]">
-      <p className="font-inter-semibold text-[22px]">Ways of Working</p>
+      <p className="font-inter font-semibold text-[22px]">Ways of Working</p>
       <div className="flex flex-row gap-[18px]">
         <ProductCarouselItem />
         <ProductCarouselItem />
--- src/components/pages/main/ProductCarouselItem.tsx
+++ src/components/pages/main/ProductCarouselItem.tsx
@@ -4,9 +4,9 @@ export default function ProductCarouselItem() {
       {/* 썸네일 */}
       <div className="w-[140px] h-[140px] bg-lightGray-4 rounded-sm"></div>
       {/* 상품명 */}
-      <p className="font-sd-gothic-medium">SS 핑크 탱크 텀블러 503ml</p>
+      <p className="font-sd-gothic font-medium">SS 핑크 탱크 텀블러 503ml</p>
       {/* 가격 */}
-      <p className="font-sd-gothic-medium font-bold">35,000원</p>
+      <p className="font-sd-gothic font-bold">35,000원</p>
     </div>
   );
 }
