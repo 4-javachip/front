16028d51f88fe640dc221a07a31ec51d40847025
Sutaenghhh <neocop7@gmail.com>
2025-03-21 11:57:04 +0900

    feat: 푸터생성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from '@/components/layouts/Header';
 import Nav from '@/components/layouts/Nav';
+import Footer from '@/components/layouts/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -34,6 +35,7 @@ export default function RootLayout({
             <Header />
 
             {children}
+            <Footer />
           </div>
         </div>
       </body>
