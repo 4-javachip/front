0bbf259f865adf4c477b10f2ce3eb0db4ed539a1
Sutaenghhh <neocop7@gmail.com>
2025-03-21 09:10:39 +0900

    style:header및 nav바 수정 (#11)

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -2,13 +2,18 @@ import Image from 'next/image';
 import SearchIcon from '@/components/ui/icons/SearchIcon';
 import CartIcon from '@/components/ui/icons/CartIcon';
 import MenuIcon from '@/components/ui/icons/MenuIcon';
-import IconButton from '@/components/ui/IconButton'; // ✅ 경로 수정
+// import IconButton from '@/components/ui/buttons/IconButton'; // ✅ 경로 수정
 import Nav from './Nav';
 
 const Header = () => {
   return (
     <header className="w-full bg-white shadow-md z-50">
-      <div className="relative w-full px-4 py-3 flex justify-between items-center">
+      <div className="relative w-full px-4  py-3 flex items-center justify-between">
+        {/* 왼쪽 - 메뉴 아이콘 */}
+        <div className="flex items-center">
+          <MenuIcon />
+        </div>
+
         {/* 로고 중앙 정렬 */}
         <div className="absolute left-1/2 transform -translate-x-1/2">
           <Image
@@ -19,22 +24,11 @@ const Header = () => {
           />
         </div>
 
-        {/* 왼쪽 - 빈 공간 (자리 맞추기) */}
-        {/* <div className="w-10"></div> */}
-        <div className="justify-start">
-          <IconButton>
-            <MenuIcon />
-          </IconButton>
-        </div>
-
         {/* 오른쪽 - 검색 & 장바구니 */}
-        <div className="flex space-x-3">
-          <IconButton className="w-6 h-6">
-            <SearchIcon />
-          </IconButton>
-          <IconButton className="w-6 h-6">
-            <CartIcon />
-          </IconButton>
+        <div className="flex items-center space-x-3">
+          <SearchIcon />
+
+          <CartIcon />
         </div>
       </div>
 
--- src/components/layouts/Nav.tsx
+++ src/components/layouts/Nav.tsx
@@ -3,24 +3,25 @@ import Link from 'next/link';
 
 const Nav = () => {
   return (
-    <nav className="w-full bg-white shadow-md">
-      <ul className="flex justify-around p-3 text-gray-700">
-        <li>
+    <nav className="w-full bg-white p-0 m-0 font-inter">
+      <ul className="flex  text-gray-700 p-0 m-0 text-sm">
+        <li className=" py-[19px] w-[25%] text-center font-semibold">
           <Link href="/">
-            <span className="hover:text-green-600">메인</span>
+            <span className="hover:text-green-600  ">메인</span>
           </Link>
         </li>
-        <li>
-          <Link href="/best">
-            <span className="hover:text-green-600">베스트</span>
-          </Link>
-        </li>
-        <li>
+        <li className="py-[19px] w-[25%] text-center font-semibold">
           <Link href="/event">
             <span className="hover:text-green-600">기획전</span>
           </Link>
         </li>
-        <li>
+        <li className="py-[19px] w-[25%] text-center font-semibold">
+          <Link href="/best">
+            <span className="hover:text-green-600 ">베스트</span>
+          </Link>
+        </li>
+
+        <li className="py-[19px] w-[25%] text-center font-semibold ">
           <Link href="/mypage">
             <span className="hover:text-green-600">마이페이지</span>
           </Link>
