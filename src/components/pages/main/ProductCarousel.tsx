e406bcad2982896f16a87edeb5c4d2b0c766bb21
unknown <doeh2580@gmail.com>
2025-03-21 10:35:34 +0900

    feat: 프로덕트 캐러셀 아이템 ui 퍼블리싱

--- src/components/pages/main/ProductCarousel.tsx
+++ src/components/pages/main/ProductCarousel.tsx
@@ -2,12 +2,15 @@ import ProductCarouselItem from './ProductCarouselItem';
 
 export default function ProductCarousel() {
   return (
-    <div className="flex flex-col container overflow-x-auto scrollbar gap-[30px]">
+    <div className="flex flex-col container gap-[30px]">
       <p className="font-inter font-semibold text-[22px]">Ways of Working</p>
-      <div className="flex flex-row gap-[18px]">
-        <ProductCarouselItem />
-        <ProductCarouselItem />
-        <ProductCarouselItem />
+      <div
+        className="flex flex-row gap-[18px] overflow-x-auto"
+        style={{ scrollbarWidth: 'none' }}
+      >
+        <ProductCarouselItem isOnSale={true} />
+        <ProductCarouselItem isOnSale={false} />
+        <ProductCarouselItem isOnSale={true} />
       </div>
     </div>
   );
--- src/components/pages/main/ProductCarouselItem.tsx
+++ src/components/pages/main/ProductCarouselItem.tsx
@@ -1,12 +1,38 @@
-export default function ProductCarouselItem() {
+import ProductLabelIcon from '@/components/ui/icons/ProductLabelIcon';
+
+interface ProductlItemType {
+  isOnSale: boolean;
+}
+
+export default function ProductCarouselItem({ isOnSale }: ProductlItemType) {
   return (
-    <div className="flex flex-col gap-3">
+    <div className="flex flex-col gap-3 w-[140px]">
       {/* 썸네일 */}
       <div className="w-[140px] h-[140px] bg-lightGray-4 rounded-sm"></div>
-      {/* 상품명 */}
-      <p className="font-sd-gothic font-medium">SS 핑크 탱크 텀블러 503ml</p>
+      <div className="flex flex-col gap-2">
+        <ProductLabelIcon isBest={true} isNew={true} />
+        {/* 상품명 */}
+        <p className="font-sd-gothic font-medium">SS 핑크 탱크 텀블러 503ml</p>
+      </div>
       {/* 가격 */}
-      <p className="font-sd-gothic font-bold">35,000원</p>
+      <div className="relative">
+        {isOnSale ? (
+          <>
+            <p className="font-sd-gothic text-lightGray-6 line-through">
+              35,000원
+            </p>
+            <p className="font-sd-gothic font-bold">31,500원</p>
+            {/* 할인률 */}
+            <p className="absolute bottom-0 right-0 font-sd-gothic text-green font-bold">
+              5%
+            </p>
+          </>
+        ) : (
+          <>
+            <p className="font-sd-gothic font-bold">31,500원</p>
+          </>
+        )}
+      </div>
     </div>
   );
 }
