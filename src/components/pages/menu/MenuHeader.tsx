a87bf610c4e5559100d0e1aa64f34fc8dbae57fd
Sutaenghhh <neocop7@gmail.com>
2025-03-22 14:31:53 +0900

    style:메뉴 헤더 완성

--- src/components/pages/menu/MenuHeader.tsx
+++ src/components/pages/menu/MenuHeader.tsx
@@ -3,17 +3,30 @@ import XIcon from '@/components/ui/icons/XIcon';
 
 export default function MenuHeader() {
   return (
-    <div className=" p-4 border-b">
-      <section>
-        <Link href="/" className="flex items-end">
+    <div>
+      {/* X 버튼 영역 */}
+      <section className="flex justify-end pb-[24px] pt-[21px] px-[32px]">
+        <Link href="/">
           <XIcon />
         </Link>
       </section>
-      <section className="px-[24px]">
-        <h2 className="text-[22px] font-semibold font-inter ">Welcome !</h2>
-        <p className="text-sm text-gray-500">
-          온라인 스토어에 오신 것을 환영합니다.
-        </p>
+
+      {/* 텍스트 + border 영역 */}
+      <section className="w-full">
+        {/* 텍스트 왼쪽 정렬 */}
+        <div className="px-[24px]">
+          <h2 className="text-[22px] font-semibold font-inter pt-[20px]">
+            Welcome !
+          </h2>
+          <p className="font-inter text-[12px] font-medium text-black pt-[12px] pb-[20px]">
+            온라인 스토어에 오신 것을 환영합니다.
+          </p>
+        </div>
+
+        {/* 가운데 정렬된 긴 border */}
+        <div className="flex justify-center">
+          <div className="w-[90%] border-b border-gray-300" />
+        </div>
       </section>
     </div>
   );
