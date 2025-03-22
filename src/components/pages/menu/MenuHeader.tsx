f69fc7520e91695c2d97f0481112a4f0bc253200
Sutaenghhh <neocop7@gmail.com>
2025-03-22 13:43:59 +0900

    feat: 메뉴 헤더 위치수정

--- src/components/pages/menu/MenuHeader.tsx
+++ src/components/pages/menu/MenuHeader.tsx
@@ -3,16 +3,18 @@ import XIcon from '@/components/ui/icons/XIcon';
 
 export default function MenuHeader() {
   return (
-    <div className="flex justify-between items-center p-4 border-b">
+    <div className=" p-4 border-b">
       <section>
-        <h2 className="text-lg font-bold">Welcome !</h2>
+        <Link href="/" className="flex items-end">
+          <XIcon />
+        </Link>
+      </section>
+      <section className="px-[24px]">
+        <h2 className="text-[22px] font-semibold font-inter ">Welcome !</h2>
         <p className="text-sm text-gray-500">
           온라인 스토어에 오신 것을 환영합니다.
         </p>
       </section>
-      <Link href="/">
-        <XIcon />
-      </Link>
     </div>
   );
 }
