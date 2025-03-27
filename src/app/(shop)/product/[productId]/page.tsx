f81d3cfbf810857d64debbc5ba77c19678a3febd
unknown <doeh2580@gmail.com>
2025-03-27 18:37:50 +0900

    refactor: 상품 상세 페이지 리팩토링 상품 id를 파라미터로 전달하도록 수정 article 태그를 삭제 파라미터로 데이터 가져오는 작업

--- src/app/(shop)/product/[productId]/page.tsx
+++ src/app/(shop)/product/[productId]/page.tsx
@@ -4,6 +4,7 @@ import ProductDesc from '@/components/pages/productDetail/ProductDesc';
 import ProductImg from '@/components/pages/productDetail/ProductImg';
 import ProductInfo from '@/components/pages/productDetail/ProductInfo';
 import { dummyProductDetails } from '@/data/dummyDatas';
+import Link from 'next/link';
 
 export default async function ProductDetail({
   params,
@@ -16,7 +17,14 @@ export default async function ProductDetail({
   );
 
   if (!product) {
-    return <p className="text-center py-10">상품을 찾을 수 없습니다.</p>;
+    return (
+      <section className="font-sd-gothic text-lightGray-6 text-center py-50">
+        <p className="pb-1">상품을 찾을 수 없습니다.</p>
+        <Link href="/" className="underline">
+          홈으로
+        </Link>
+      </section>
+    );
   }
 
   return (
