7a5c40978f50881e69f3280f95d4dda2a8bdadf5
Sutaenghhh <neocop7@gmail.com>
2025-03-27 17:13:04 +0900

    style: 메뉴 모달창 링크 이동시 자동으로 닫히게 수정 , 메뉴 bannerlist 컴포넌트 분리하여 유지보수 쉽게 수정 (#72)
    * style: 메뉴 모달창 링크 이동시 자동으로 닫히게 수정 , 메뉴 bannerlist 컴포넌트 분리하여 유지보수 쉽게 수정
    
    * refacator: px-> rem으로 변경

--- src/components/pages/menu/CategoryItem.tsx
+++ src/components/pages/menu/CategoryItem.tsx
@@ -1,13 +1,19 @@
 import { CategoryMenuType } from '@/types/ResponseDataTypes';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface Props {
   category: CategoryMenuType;
+  onClose: () => void;
 }
 
-export default function CategoryItem({ category }: Props) {
+export default function CategoryItem({ category, onClose }: Props) {
   return (
-    <article className="flex flex-col items-center">
+    <Link
+      href={`/products?category=${category.id}`}
+      onClick={onClose}
+      className="flex flex-col items-center"
+    >
       <figure className="w-25 h-25 bg-gray-100 rounded-full flex items-center justify-center overflow-hidden shadow-md">
         <Image
           src={category.thumbnail.imageUrl}
@@ -21,6 +27,6 @@ export default function CategoryItem({ category }: Props) {
       <figcaption className="text-sm  mt-[10px] font-inter font-medium text-[14px] text-black">
         {category.name}
       </figcaption>
-    </article>
+    </Link>
   );
 }
--- src/components/pages/menu/MenuCategoryList.tsx
+++ src/components/pages/menu/MenuCategoryList.tsx
@@ -2,17 +2,20 @@ import RightArrowIcon from '@/components/ui/icons/RightArrowIcon';
 import { CategoryMenuType } from '@/types/ResponseDataTypes';
 import Link from 'next/link';
 import CategoryItem from './CategoryItem';
+import { on } from 'events';
 
 interface Props {
   categories: CategoryMenuType[];
+  onClose: () => void;
 }
 
-export default function MenuCategoryList({ categories }: Props) {
+export default function MenuCategoryList({ categories, onClose }: Props) {
   return (
     <section className="px-6 py-7">
       <nav className="flex justify-end pb-4.5">
         <Link
           href="/products"
+          onClick={onClose}
           className="text-xs font-inter text-[#2D2D2D] flex items-center space-x-1"
         >
           <span>전체 상품 보기</span>
@@ -26,7 +29,7 @@ export default function MenuCategoryList({ categories }: Props) {
       >
         {categories.map((category) => (
           <li key={category.id}>
-            <CategoryItem category={category} />
+            <CategoryItem category={category} onClose={onClose} />
           </li>
         ))}
       </ul>
