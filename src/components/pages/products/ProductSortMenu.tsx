0066b52452fe316785400ff29c152dab4c51cb58
yunchyn <119782741+yunchyn@users.noreply.github.com>
2025-03-28 12:00:24 +0900

    fix: 빌드 오류 해결 (#87)
    useSearchParams를 Suspense로 감싸도록 수정

--- src/components/pages/products/ProductSortMenu.tsx
+++ src/components/pages/products/ProductSortMenu.tsx
@@ -1,10 +1,13 @@
 'use client';
 import DropDownModal from '@/components/ui/dropdown/DropDownModal';
+import { Suspense } from 'react';
 
 export default function ProductSortMenu() {
   return (
     <section className="padded pt-4 flex justify-end">
-      <DropDownModal />
+      <Suspense fallback={<div></div>}>
+        <DropDownModal />
+      </Suspense>
     </section>
   );
 }
