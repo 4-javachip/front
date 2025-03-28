ecf98e52c1895c0809d307a78192a71fad24d710
yunchyn <119782741+yunchyn@users.noreply.github.com>
2025-03-28 09:14:23 +0900

    feat: Noto sans KR 폰트 추가 (#79)
    PageNavBar에 적용

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,14 @@
+import { LogoIcon } from '@/components/ui/icons/LogoIcon';
+
+export default function SignInPage() {
+  return (
+    <main>
+      <LogoIcon />
+      <div className="font-inter font-semibold text-2xl">
+        <h2>안녕하세요.</h2>
+        <h2>스타벅스입니다.</h2>
+      </div>
+      <p>회원 서비스 이용을 위해 로그인 해주세요.</p>
+    </main>
+  );
+}
