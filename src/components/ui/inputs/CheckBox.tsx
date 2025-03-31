230ebaf9d1c37d5b572950df2dc9384e00c6710a
Sutaenghhh <neocop7@gmail.com>
2025-03-31 09:43:38 +0900

    refactor: 장바구니 체크박스 css 변경

--- src/components/ui/inputs/CheckBox.tsx
+++ src/components/ui/inputs/CheckBox.tsx
@@ -15,7 +15,12 @@ export default function Checkbox({
       type="checkbox"
       checked={checked}
       onChange={onChange}
-      className={`accent-green-600 ${className}`}
+      className={`terms-checkbox
+      w-5 h-5 appearance-none border border-green rounded
+    flex items-center justify-center 
+    checked:before:content-['✔'] checked:before:text-white checked:before:text-sm
+    checked:bg-green checked:border-transparent
+    active:border-black${className}`}
     />
   );
 }
