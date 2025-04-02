3225be9fec244916549007b60cbebb195f5000f2
Sutaenghhh <neocop7@gmail.com>
2025-04-02 17:22:40 +0900

    refactor:mypage

---
+++ src/components/pages/mypage/MyPageMenu.tsx
@@ -0,0 +1,33 @@
+import AddressAgreementIcon from '@/components/ui/icons/AddressAgreement';
+import { CouponIcon } from '@/components/ui/icons/CouponIcon';
+import OrderListIcon from '@/components/ui/icons/OrderListIcon';
+import ShippingAddressIcon from '@/components/ui/icons/ShippingAddressIcon';
+
+export default function MyPageMenu() {
+  return (
+    <section className="h-120 w-full bg-lightGray-2">
+      <ul className="px-6 font-body ">
+        <li className=" font-semibold pt-7.5 pb-5.5">쇼핑정보</li>
+      </ul>
+
+      <ul className="px-6">
+        <li className="font-medium text-foreground flex gap-2 pb-8">
+          <OrderListIcon /> 주문 내역
+        </li>
+        <li className="font-medium text-foreground flex gap-2 pb-8">
+          <CouponIcon /> 쿠폰
+        </li>
+        <li className="font-medium text-foreground flex gap-2 pb-8">
+          <ShippingAddressIcon /> 배송지 관리
+        </li>
+      </ul>
+
+      <ul className="px-6 font-semibold pt-2">
+        <li className=" font-semibold pt-2.2 pb-5.5">설정</li>
+        <li className="font-medium text-foreground flex gap-2 pb-8">
+          <AddressAgreementIcon /> 배송지 정보 수집 및 이용 동의
+        </li>
+      </ul>
+    </section>
+  );
+}
---
+++ src/components/pages/mypage/OrderDeliveryStatus.tsx
@@ -0,0 +1,38 @@
+import RightArrowIcon from '@/components/ui/icons/RightArrowIcon';
+
+export default function OrderDeliveryStatus() {
+  const statusList = [
+    { label: '결제완료', count: 0 },
+    { label: '상품준비중', count: 0 },
+    { label: '배송중', count: 0 },
+    { label: '배송완료', count: 0 },
+  ];
+  return (
+    <section>
+      <ul className="flex justify-between items-center mb-4 pt-7 px-6 font-body">
+        <li className=" font-semibold flex items-center gap-2">
+          주문/배송현황
+        </li>
+        <li className="text-lightGray-1 text-xs">최근 3개월동안 구매한 상품</li>
+      </ul>
+
+      <ul className="flex items-center justify-between text-center pt-6 pb-5">
+        {statusList.map((status, idx) => (
+          <div key={status.label} className="flex-1 relative">
+            <li className="text-2xl font-semibold font-inter text-lightGray-4">
+              {status.count}
+            </li>
+            <li className="mt-2 text-sm font-medium text-foreground">
+              {status.label}
+            </li>
+            {idx < statusList.length - 1 && (
+              <li className="absolute right-0 top-1/2 transform translate-x-1/2 -translate-y-1/2 ">
+                <RightArrowIcon />
+              </li>
+            )}
+          </div>
+        ))}
+      </ul>
+    </section>
+  );
+}
