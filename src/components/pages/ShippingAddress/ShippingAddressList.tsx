1468f352177bcf8ba2dd7aeedee523ff46e0194d
Sutaenghhh <neocop7@gmail.com>
2025-04-10 09:54:45 +0900

    refactor: 배송지 목록에서 기본 배송지가 맨위에 나오게 수정

--- src/components/pages/ShippingAddress/ShippingAddressList.tsx
+++ src/components/pages/ShippingAddress/ShippingAddressList.tsx
@@ -22,6 +22,12 @@ export default function ShippingAddressList({
   const addressLength = address.length;
   const router = useRouter();
 
+  const sortedAddress = address.sort((a, b) => {
+    if (a.addressList.defaulted && !b.addressList.defaulted) return -1;
+    if (!a.addressList.defaulted && b.addressList.defaulted) return 1;
+    return 0;
+  });
+
   const handleDelete = async (shippingAddressUuid: string) => {
     try {
       await deleteShippingAddress(shippingAddressUuid);
@@ -36,13 +42,13 @@ export default function ShippingAddressList({
           <EmptyAddress />
         ) : (
           <ul className="space-y-4">
-            {address.map(({ address, addressList }, index) => (
+            {sortedAddress.map(({ address, addressList }, index) => (
               <li key={addressList.shippingAddressUuid}>
-                <article className="flex justify-between items-start pb-4">
+                <article className="flex justify-between items-start pb-4 ">
                   <div className="flex flex-col gap-1 w-60 font-body">
                     <ul className="flex items-center  gap-2">
                       <li>
-                        <strong className="text-basefont-semibold text-lightGray-12">
+                        <strong className="text-basefont-semibold text-lightGray-12 leading-7">
                           {address.addressName} ({address.recipientName})
                         </strong>
                       </li>
@@ -55,12 +61,12 @@ export default function ShippingAddressList({
                       )}
                     </ul>
 
-                    <address className="not-italic font-light text-xs text-foreground">
+                    <address className="not-italic font-light text-sm text-foreground">
                       ({address.zipCode}) {address.baseAddress}
                       <br />
                       {address.detailAddress}
                     </address>
-                    <ul className="flex items-center gap-1 font-pretendard text-xs text-lightGray-7">
+                    <ul className="flex items-center gap-2 font-pretendard text-xs text-lightGray-7 leading-7">
                       <li>{address.phoneNumber}</li>
                       <li className="border-l-2 pl-2 leading-3">
                         {address.secondPhoneNumber}
@@ -68,16 +74,15 @@ export default function ShippingAddressList({
                     </ul>
                   </div>
 
-                  <nav className="flex items-center pl-2 gap-2">
+                  <nav className="flex items-center pl-2 gap-2 font-body text-xs text-lightGray-7">
                     <Link
                       href={`/shipping-addresses/${addressList.shippingAddressUuid}`}
-                      className="text-sm text-lightGray-7 px-2"
                     >
                       수정
                     </Link>
                     {!addressList.defaulted && (
                       <button
-                        className="border-l-2 border-lightGray-5 pl-2 leading-3 text-lightGray-7"
+                        className="border-l-2 border-lightGray-5 pl-2 leading-3"
                         onClick={() =>
                           handleDelete(addressList.shippingAddressUuid)
                         }
