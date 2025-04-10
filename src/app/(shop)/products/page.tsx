e6d4313b11569c42b883161b30398ed2821d51d1
yunchyn <119782741+yunchyn@users.noreply.github.com>
2025-04-10 18:39:38 +0900

    feat: products 페이지 무한 스크롤 (#188)
    * feat: 무한 스크롤 초기 세팅
    
    * feat: 변경된 api에 맞게 product가 무한 스크롤 되도록 api 연결
    
    * feat: product item 및 썸네일, 옵션 skeleton ui 추가
    
    * feat: 마이페이지 정보란 추가
    
    * fix: 무한 스크롤 page를 url 파라미터에 포함시키도록 변경 및 데이터 페칭을 client side인 productList에서 하도록 변경

--- src/app/(shop)/products/page.tsx
+++ src/app/(shop)/products/page.tsx
@@ -1,23 +1,29 @@
 import { getProductListData } from '@/actions/product-service';
 import ProductList from '@/components/pages/products/ProductList';
 import ProductSortMenu from '@/components/pages/products/ProductSortMenu';
+import ProductItemSkeleton from '@/components/ui/skeletons/ProductItemSkeleton';
+import { Suspense } from 'react';
 
-export default async function ProductListPage({
-  searchParams,
-}: {
-  searchParams: Promise<{ page: string }>;
-}) {
-  const { page } = await searchParams;
-  const pageSize = 4;
-  const productsData = await getProductListData({
-    pageSize: Number(pageSize),
-    page: Number(page) || 1,
-  });
+export default async function ProductListPage() {
+  //   {
+  //   searchParams,
+  // }: {
+  //   searchParams: Promise<{ page: string }>;
+  // }
+  // const { page } = await searchParams;
+  // console.log(page);
+  // const pageSize = 8;
 
   return (
     <main>
       <ProductSortMenu />
-      <ProductList initialProducts={productsData} />
+      {/* <Suspense
+        fallback={Array.from({ length: pageSize }).map((_, index) => (
+          <ProductItemSkeleton size={800} key={index} />
+        ))}
+      > */}
+      <ProductList />
+      {/* </Suspense> */}
     </main>
   );
 }
--- src/components/pages/products/ProductList.tsx
+++ src/components/pages/products/ProductList.tsx
@@ -1,61 +1,69 @@
 'use client';
 
-import {
-  PaginatedResponseType,
-  ProductNameDataType,
-} from '@/types/ProductResponseDataTypes';
+import { ProductNameDataType } from '@/types/ProductResponseDataTypes';
 import ProductlItem from '../../ui/productItem/ProductItem';
 import React, { useEffect, useRef, useState } from 'react';
-import ProductItemSkeleton from '@/components/ui/skeletons/ProductItemSkeleton';
 import { getProductListData } from '@/actions/product-service';
 import Loader from '@/components/ui/loader';
+import { useSearchParams, useRouter } from 'next/navigation';
 
-export default function ProductList({
-  initialProducts,
-}: {
-  initialProducts: PaginatedResponseType<ProductNameDataType[]>;
-}) {
+export default function ProductList() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const pageSize = 6;
+  const loaderRef = useRef<HTMLDivElement | null>(null);
+
+  const initialPage = Number(searchParams.get('page')) || 1;
+
+  const [page, setPage] = useState(initialPage);
+  const [products, setProducts] = useState<ProductNameDataType[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [products, setProducts] = useState(initialProducts.content);
-  const [page, setPage] = useState(2);
-  const [pageSize, setPageSize] = useState(initialProducts.pageSize);
-  const [hasNext, setHasNext] = useState(initialProducts.hasNext);
-
-  const loaderRef = useRef<HTMLDivElement>(null);
-
-  const loadMore = async () => {
-    if (!hasNext) return;
-
-    setIsLoading(true);
-    const newData = await getProductListData({
-      pageSize: pageSize,
-      page: page,
-    });
-    console.log(page, newData);
-    setProducts((prev) => [...prev, ...newData.content]);
-    setPage((prev) => prev + 1);
-    setHasNext(newData.hasNext);
-    setIsLoading(false);
-  };
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].isIntersecting) {
-        loadMore();
-      }
-    });
+    const fetchData = async () => {
+      setIsLoading(true);
 
-    const ref = loaderRef.current;
-    if (ref) observer.observe(ref);
+      const res = await getProductListData({ pageSize, page });
+
+      // 중복 제거
+      // const newItems = res.content.filter(
+      //   (item) => !products.some((p) => p.productUuid === item.productUuid)
+      // );
+
+      setProducts((prev) => [...prev, ...res.content]);
+      setHasMore(res.hasNext);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [page]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting && !isLoading && hasMore) {
+          const nextPage = page + 1;
+          setPage(nextPage);
+          router.push(`/products?page=${nextPage}`, { scroll: false });
+        }
+      },
+      { threshold: 1 }
+    );
+
+    const current = loaderRef.current;
+    if (current) observer.observe(current);
 
     return () => {
-      if (ref) observer.unobserve(ref);
+      if (current) observer.unobserve(current);
     };
-  }, [loaderRef, page, hasNext]);
+  }, [loaderRef, isLoading, hasMore, page, router]);
 
   return (
     <section className="padded py-6 flex justify-center flex-col">
-      <ul className="w-full grid grid-cols-2 gap-4">
+      <ul className="w-full grid grid-cols-2 gap-4 min-h-[80vh]">
         {products.map((product) => (
           <ProductlItem
             key={product.productUuid}
@@ -63,16 +71,10 @@ export default function ProductList({
             size={800}
           />
         ))}
-        {isLoading &&
-          Array.from({ length: pageSize }).map((_, index) => (
-            <ProductItemSkeleton size={800} key={index} />
-          ))}
       </ul>
-      {hasNext && (
-        <div ref={loaderRef} className="h-10 pt-3 w-full flex justify-center">
-          <Loader size="8" />
-        </div>
-      )}
+      <div ref={loaderRef} className="h-10 pt-10 w-full flex justify-center">
+        {isLoading && <Loader size="8" />}
+      </div>
     </section>
   );
 }
