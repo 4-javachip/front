a3869483d4b3edeb1d239970ad1b97958d5bfd14
Sutaenghhh <neocop7@gmail.com>
2025-04-13 23:00:22 +0900

    feat : 배송지 이용약관 상태 변경 및 배송지 삭제 시 확인 모달 추가  (#197)
    * feat: 배송지 삭제 시 delete 모달로 확인 후 삭제
    
    * feat: 배송지 정보수집 및 이용 동의 해제 시 확인 모달 후 약관 비동의 및 배송지 목록 삭제
    
    * fix : 배송약관 스위치를 fals-> true 로 변경시 api 호출 안되는 오류 수정

--- src/components/pages/shippingaddress-agreement/ShippingAddressAgreementHeader.tsx
+++ src/components/pages/shippingaddress-agreement/ShippingAddressAgreementHeader.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { userAgreement } from '@/actions/agreement-service';
 import { CommonLayout } from '@/components/layouts/CommonLayout';
+import ConfirmModal from '@/components/ui/ConfirmModal';
 import { Switch } from '@/components/ui/switch';
 
 import TextTitleH1 from '@/components/ui/texts/TextTitleH1';
@@ -14,14 +15,38 @@ export default function ShippingAddressAgreementHeader({
   userAgreed: UserAgreementType[];
   agreementId: number;
 }) {
+  const [loading, setLoading] = useState(false);
   const [isCheck, setIsCheck] = useState(false);
   console.log('userAgreed', userAgreed);
+  const [openModal, setOpenModal] = useState(false);
   const handleClick = async () => {
-    console.log('handleClick', isCheck);
-    await userAgreement({
-      agreementId: agreementId,
-      agreed: !isCheck,
-    });
+    if (isCheck) {
+      setOpenModal(true);
+    } else {
+      try {
+        setLoading(true);
+        await userAgreement({ agreementId, agreed: true });
+        setIsCheck(true);
+      } catch (err) {
+        console.error('동의 처리 실패', err);
+        alert('동의 처리에 실패했습니다.');
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+  const handleDisagreeConfirm = async () => {
+    try {
+      await userAgreement({
+        agreementId,
+        agreed: false,
+      });
+      setIsCheck(false);
+    } catch (err) {
+      console.error('약관 비동의 실패:', err);
+    } finally {
+      setOpenModal(false);
+    }
   };
 
   useEffect(() => {
@@ -35,8 +60,18 @@ export default function ShippingAddressAgreementHeader({
           <TextTitleH1>배송지 정보 수집 및 이용 동의</TextTitleH1>
         </li>
         <div className="flex items-center justify-center px-6 ">
-          <Switch checked={isCheck} onClick={handleClick} />
+          <Switch checked={isCheck} onClick={handleClick} disabled={loading} />
         </div>
+        <ConfirmModal
+          open={openModal}
+          onOpenChange={setOpenModal}
+          title="배송지 정보 수집 및 이용 동의"
+          description="배송지 정보 수집 및 이용 동의를 해제하시겠습니까?
+           해제 시 저장된 배송지 정보가 모두 삭제됩니다"
+          confirmText="동의하지 않음"
+          cancelText="취소"
+          onConfirm={handleDisagreeConfirm}
+        />
       </ul>
       <CommonLayout.SectionInnerPadding>
         <CommonLayout.CommonBorder />
