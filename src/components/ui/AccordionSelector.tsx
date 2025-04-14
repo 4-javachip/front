4b0bdb96afed169e1704274ad4ba09c483eaeadf
unknown <doeh2580@gmail.com>
2025-04-14 14:57:27 +0900

    feat: AccordionSelector 추가

--- src/components/ui/AccordionSelector.tsx
+++ src/components/ui/AccordionSelector.tsx
@@ -7,12 +7,19 @@ import {
 
 export default function AccordionSelector() {
   return (
-    <Accordion type="single" collapsible>
+    <Accordion
+      type="single"
+      collapsible
+      defaultValue="item-1"
+      className="w-full border-2 border-lightGray-8 rounded-md"
+    >
       <AccordionItem value="item-1">
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
+        <AccordionTrigger className="padded text-[0.9375rem] text-black hover:no-underline hover:black">
+          색상
+        </AccordionTrigger>
+        <hr />
+        <AccordionContent>11</AccordionContent>
+        <AccordionContent>22</AccordionContent>
       </AccordionItem>
     </Accordion>
   );
