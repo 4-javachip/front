ecd79a8859fb716edf2d282f4932f053ac764bec
Sutaenghhh <neocop7@gmail.com>
2025-04-20 14:03:04 +0900

    refactor:  결제 생성 시 주문 명 변경  (#251)
    * feat: 결제 api 연동
    
    * feat: 결제 성공 실패 페이지 생성
    
    * refactor: 결제 시 주문 명 변경
    
    * refactor: 필요없는 주석 제거

--- src/components/pages/order/OrderPurchaseBar.tsx
+++ src/components/pages/order/OrderPurchaseBar.tsx
@@ -5,17 +5,13 @@ import { CommonLayout } from '@/components/layouts/CommonLayout';
 import CommonButton from '@/components/ui/buttons/CommonButton';
 import { useOrderItemContext } from '@/context/OrderItemContext';
 
-// import { useRouter } from 'next/navigation';
-
 export default function OrderPurchaseBar() {
-  // const router = useRouter();
   const { paymentData } = useOrderItemContext();
   console.log('paymentData', paymentData);
   const handleClick = async () => {
     try {
       const checkoutUrl = await PaymentData(paymentData);
       window.location.href = checkoutUrl;
-      console.log('checkouturl', checkoutUrl);
     } catch (err) {
       console.error('결제 요청 에러:', err);
       alert(
