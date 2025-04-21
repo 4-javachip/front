c6e66c118743eb79ab3ae1093762da9183d8cd63
Sutaenghhh <neocop7@gmail.com>
2025-04-21 11:56:16 +0900

    Update page.tsx

--- src/app/(orderpayment)/payment/success/page.tsx
+++ src/app/(orderpayment)/payment/success/page.tsx
@@ -6,7 +6,7 @@ import { usePaymentSuccessContext } from '@/context/PaymentSuccessContext';
 import { PaymentSuccessReturnType } from '@/types/PaymentDataType';
 import { OrderListData } from '@/actions/order-service';
 import Image from 'next/image';
-import PaymentPurchaseBar from '@/components/pages/payment-success/paymentPurchasebar';
+import PaymentPurchaseBar from '@/components/pages/payment-success/PaymentPurchasebar';
 
 export default function PaymentSuccessPage() {
   const params = useSearchParams();
