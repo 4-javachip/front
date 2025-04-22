dd7cd755130533e40a4234ec977fb1de036b8d84
Sutaenghhh <neocop7@gmail.com>
2025-04-22 11:25:21 +0900

    refactor:백업

--- src/actions/payments-service/index.ts
+++ src/actions/payments-service/index.ts
@@ -2,7 +2,7 @@
 import { options } from '@/app/api/auth/[...nextauth]/options';
 import { PaymentPayload } from '@/context/OrderItemContext';
 
-import { PaymentReturnType } from '@/types/PaymentDataType';
+import { paymentConfirmData, PaymentReturnType } from '@/types/PaymentDataType';
 import { CommonResponseType } from '@/types/ResponseDataTypes';
 import { getServerSession } from 'next-auth';
 
@@ -20,6 +20,32 @@ export const PaymentData = async (PaymentData: PaymentPayload) => {
     },
     body: JSON.stringify(PaymentData),
   });
+  if (!res.ok) {
+    const errorData = await res.json();
+    throw new Error(errorData.message || '주문내역목록 조회 실패');
+  }
+  const data = (await res.json()) as CommonResponseType<PaymentReturnType>;
+  return data.result;
+};
+
+export const PaymentconfirmData = async (
+  PaymentconfirmData: paymentConfirmData
+) => {
+  const session = await getServerSession(options);
+  const token = session?.user.accessToken || session?.user.refreshToken;
+  console.log('리프레쉬 토큰 ', session?.user.refreshToken);
+  console.log('토큰 ', token);
+  const res = await fetch(
+    `${process.env.BASE_API_URL}/api/v1/payment/confirm`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`,
+      },
+      body: JSON.stringify(PaymentconfirmData),
+    }
+  );
   if (!res.ok) {
     const errorData = await res.json();
     throw new Error(errorData.message || '주문내역목록 조회 실패');
