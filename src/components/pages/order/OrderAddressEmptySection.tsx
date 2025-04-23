b58a0ebbd4bc5bde072e49e165d6811ddf5820c9
Sutaenghhh <neocop7@gmail.com>
2025-04-23 11:58:39 +0900

    refactor: 주문 페이지 배송지 emptysection 오류 수정

--- src/components/pages/order/OrderAddressEmptySection.tsx
+++ src/components/pages/order/OrderAddressEmptySection.tsx
@@ -4,7 +4,7 @@ import AddressRegisterButton from '@/components/ui/buttons/AddressRegisterButton
 
 export default function OrderAddressEmptySection() {
   return (
-    <section className="flex text-center pt-6">
+    <section className="flex flex-col items-center justify-center text-center pt-6">
       <article className="text-black text-[0.9375rem] leading-relaxed mb-6 font-pretendard font-medium">
         <p>등록된 배송지가 없습니다.</p>
         <p>배송지를 등록해주세요.</p>
