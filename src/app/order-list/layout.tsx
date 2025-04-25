e2380a08ff6140d77316661539b32b62f62d9527
Sutaenghhh <neocop7@gmail.com>
2025-04-25 09:20:12 +0900

    refactor: 주문 상세내역 헤더 변경 및 paymentSuccess페이지 pb 추가 (#334)

--- src/app/order-list/layout.tsx
+++ src/app/order-list/layout.tsx
@@ -1,13 +1,10 @@
-import { CommonLayout } from '@/components/layouts/CommonLayout';
-import PageHeader from '@/components/layouts/PageHeader';
+import HeaderTop from '@/components/layouts/header/HeaderTop';
 import React from 'react';
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <CommonLayout.CommonHeader>
-        <PageHeader title="주문 내역" />
-      </CommonLayout.CommonHeader>
+      <HeaderTop />
       <div className="pt-15">{children}</div>
     </>
   );
